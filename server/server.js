25d5729c7642a174ef6bad166a4081f2ddb823b7
Emily Cai <ecai32@gatech.edu>
2022-06-15 15:14:10 -0400

    Started drafting user login info

--- server/server.js
+++ server/server.js
@@ -1,8 +1,42 @@
 const express = require("express")
 const app = express()
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
-app.use(express.static("public"))
+
+
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "LoginSystem",
+});
+
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err)
+        }
+
+        db.query(
+            "INSERT INTO users (username, password) VALUES (?,?)",
+            [username, hash],
+            (err, result) => {
+                console.log(err);
+            }
+        );
+    })
+});
+
+
+/*app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -11,7 +45,7 @@ app.set("view engine", "ejs")
 app.get("/", logger, (req, res) =>{
     // res.json({ "users": ["userOne", "userTwo", "userThree", "userFour"] })
     res.render("index", {text: "cheese"})
-})
+})*/
 
 const userRouter = require("./routes/users")
 
