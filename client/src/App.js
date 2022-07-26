41d846145ce7c99fcc4939d52fb32945fb763d51
Joshua Chio <jchio@gatech.edu>
2022-07-26 17:56:24 -0500

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import MyForm from './components/MyForm'
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
-import Home from './components/Home'; 
 
 const App = () => {
   // const navigate = useNavigate();
   return (  
     <div className="App"> 
-      {/* <MyForm /> */}
-      {/* <button onClick={() => navigate.push("/flights")}> Flights </button> */}
-      {/* <button onClick={() => navigate.push("/")}> Home Page </button> */}
       <h1>Home Page</h1>
       <Router>
         <Routes>
           <Route path="/flights" element={<MyForm />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<MyForm />} />
         </Routes>
       </Router>
       
--- server/routes/flights.js
+++ server/routes/flights.js
@@ -7,15 +7,6 @@ var amadeus = new Amadeus({
     clientSecret: 'LxTQjJGs4B6SamHk'
   });
 
-router.post("/test", (req, res) => {
-    res.send("worked")
-    console.log(req.body)
-})
-
-// router.post("/", (req, res) => {
-//     res.send("newPost")
-// })
-
 router.get("/", (req, res) => {
     res.json()
 })
@@ -31,59 +22,12 @@ router.post("/", (req, res) => {
     })
 })
 
-// router
-//     .route("/:id")
-//     .get((req, res) => {
-//         var flightData = getFlights(req, res)
-//         console.log(flightData)
-//         res.json(flightData)
-//     })
-//     .put((req, res) => {
-//         res.send('update airport Get with id ' + req.params.airportID)
-//     })
-//     .delete((req, res) => {
-//         res.send('delete airport Get with id ' + req.params.airportID)
-//     })
-
-router.get("/testing", (req, res) => {
-    res.send("dummy")
-})
-
-router.put("/testing", (req, res) => {
-    console.log("testing")
-    res.json("put new name")
-})
-
-const airports = [{ Airport: "Joshua" }, { Airport: "Emily" }]
-
-search = {orgLoc: '', destLoc: '', departDate: ''}
-
-router.param("id", (req, res, next, id) => {
-    req.airport = airports[id]
-    next()
-})
-
 const getFlightList = async (req) => {
     const flights = await getFlights(req)
     return flights
 };
 
-// let myPromise = new Promise(function(resolve, reject, req, res) {
-//     // "Producing Code" (May take some time)
-//     amadeus.shopping.flightOffersSearch.get({
-//         originLocationCode: req.from,
-//         destinationLocationCode: req.destination,
-//         departureDate: req.departureDate,
-//         adults: '1'
-//     })
-//       resolve(response.data); // when successful
-//       reject();  // when error
-// });
-
-// "Consuming Code" (Must wait for a fulfilled Promise)
-
-
-//function to get flights
+// function to get flights
 async function getFlights(req, res) {
     const fl = amadeus.shopping.flightOffersSearch.get({
         originLocationCode: req.from,
@@ -92,81 +36,6 @@ async function getFlights(req, res) {
         adults: '1'
     })
     return fl
-    // .then(function(response){
-    //     // console.log(response.data);
-    //     return response.data;
-    // }).catch(function(responseError){
-    //     console.log(responseError);
-    // });
 }
 
 module.exports = router
-
-
-//This is the output this above will give
-
-// { type: 'flight-offers-pricing',
-//   flightOffers:
-//    [ { type: 'flight-offer',
-//        id: '1',
-//        source: 'GDS',
-//        instantTicketingRequired: false,
-//        nonHomogeneous: false,
-//        paymentCardRequired: false,
-//        lastTicketingDate: '2022-06-16',
-//        itineraries:
-//         [ { segments:
-//              [ { departure:
-//                   { iataCode: 'DFW', terminal: 'E', at: '2022-08-01T13:03:00' },
-//                  arrival:
-//                   { iataCode: 'ATL', terminal: 'N', at: '2022-08-01T16:20:00' },
-//                  carrierCode: 'F9',
-//                  number: '1550',
-//                  aircraft: { code: '32N' },
-//                  operating: { carrierCode: 'F9' },
-//                  duration: 'PT2H17M',
-//                  id: '33',
-//                  numberOfStops: 0,
-//                  co2Emissions: [ { weight: 114, weightUnit: 'KG', cabin: 'ECONOMY' } ] } ] } ],
-//        price:
-//         { currency: 'EUR',
-//           total: '96.47',
-//           base: '77.00',
-//           fees:
-//            [ { amount: '0.00', type: 'SUPPLIER' },
-//              { amount: '0.00', type: 'TICKETING' },
-//              { amount: '0.00', type: 'FORM_OF_PAYMENT' } ],
-//           grandTotal: '96.47',
-//           billingCurrency: 'EUR' },
-//        pricingOptions:
-//         { fareType: [ 'PUBLISHED' ], includedCheckedBagsOnly: false },
-//        validatingAirlineCodes: [ 'F9' ],
-//        travelerPricings:
-//         [ { travelerId: '1',
-//             fareOption: 'STANDARD',
-//             travelerType: 'ADULT',
-//             price:
-//              { currency: 'EUR',
-//                total: '96.47',
-//                base: '77.00',
-//                taxes:
-//                 [ { amount: '4.22', code: 'ZP' },
-//                   { amount: '5.25', code: 'AY' },
-//                   { amount: '5.78', code: 'US' },
-//                   { amount: '4.22', code: 'XF' } ],
-//                refundableTaxes: '19.47' },
-//             fareDetailsBySegment:
-//              [ { segmentId: '33',
-//                  cabin: 'ECONOMY',
-//                  fareBasis: 'S21NXS2',
-//                  class: 'S' } ] } ] } ],
-//   bookingRequirements:
-//    { emailAddressRequired: true,
-//      mobilePhoneNumberRequired: true,
-//      travelerRequirements:
-//       [ { travelerId: '1',
-//           genderRequired: true,
-//           dateOfBirthRequired: true,
-//           redressRequiredIfAny: true,
-//           residenceRequired: true } ] } }
-
