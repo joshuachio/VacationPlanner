aca6cdc4b6a061a2f83af477aa34d1e139942b80
Joshua Chio <jchio@gatech.edu>
2022-07-28 16:48:33 -0500

    Finalize algo

--- server/routes/flights.js
+++ server/routes/flights.js
@@ -39,49 +39,63 @@ async function getFlights(req, res) {
     return fl
 }
 
+
+//Supported airports for cheapest path search
 const flightGrid = [["SEA", "SLC", "DEN", "ORD", "JFK"],
                     ["SFO", "LAS", "DFW", "BNA", "DCA"],
                     ["LAX", "PHX", "IAH", "ATL", "MIA"]]
 
 const findCheapestFlight = (from, to) => {
-
+    //Airport is not supported
     if (!(flightGrid.some(row => row.includes(from)) && flightGrid.some(row => row.includes(to)))) {
         return "Invalid airport code"
     }
 
+    //Find index of from and to coordinates
     let fromR = flightGrid.findIndex(row => row.includes(from))
     let fromC = flightGrid[fromR].indexOf(from)
-
     let toR = flightGrid.findIndex(row => row.includes(to))
     let toC = flightGrid[toR].indexOf(to)
-    
-    const airportGrid = flightGrid.slice(Math.min(fromR, toR), Math.max(fromR, toR) + 1).map(row => row.slice(Math.min(fromC, toC), Math.max(fromC, toC) + 1))
 
+    //Creates a slice of array with relevant airports
+    const airportGrid = flightGrid.slice(Math.min(fromR, toR), Math.max(fromR, toR) + 1).map(row => row.slice(Math.min(fromC, toC), Math.max(fromC, toC) + 1))
     const ROWS = airportGrid.length
     const COLS = airportGrid[0].length
 
+    //Price array with same dimensions
     let priceGrid = Array.from(Array(ROWS), () => new Array(COLS))
+    priceGrid[fromR][fromC] = Infinity
 
+    //Finds new coordinates
     fromR = airportGrid.findIndex(row => row.includes(from))
     fromC = airportGrid[fromR].indexOf(from)
-
     toR = airportGrid.findIndex(row => row.includes(to))
     toC = airportGrid[toR].indexOf(to)
 
+    //Starter path with the origin airport
     let path = [airportGrid[fromR][fromC]]
 
-    priceGrid[fromR][fromC] = Infinity
+    //Nonstop price from origin to destination
+    let curPrice = findCost(from , to)
 
-    return findPath(airportGrid, priceGrid, path, ROWS, COLS, fromR, fromC, toR, toC, 0)
+    //Finds cheapest path
+    let result = findPath(airportGrid, priceGrid, path, ROWS, COLS, fromR, fromC, toR, toC, 0)
 
+    if (result[1] > curPrice) {
+        path = result[0]
+        curPrice = result[1]
+    }
+
+    return [path, curPrice]
 }
 
 const findPath = (airportGrid, priceGrid, path, ROWS, COLS, fromR, fromC, toR, toC, total) => {
-
+    //Exit condition when the path is complete
     if (fromR === toR && fromC === toC) {
         return [path, total]
     }
 
+    //Calcaultes cost for each possible path
     for (let r = 0; r < ROWS; r++) {
         for (let c = 0; c < COLS; c++) {
             if (!(path.includes(airportGrid[r][c]))) {
@@ -104,10 +118,12 @@ const findPath = (airportGrid, priceGrid, path, ROWS, COLS, fromR, fromC, toR, t
         }
     }
 
+    //Add the cheapest path to the path array
     path.push(airportGrid[minR][minC])
     priceGrid[minR][minC] = Infinity
-    return findPath(airportGrid, priceGrid, path, ROWS, COLS, minR, minC, toR, toC, total + min)
 
+    //Recursive call to find the cheapest path
+    return findPath(airportGrid, priceGrid, path, ROWS, COLS, minR, minC, toR, toC, total + min)
 }
 
 const findCost = (from, to) => {
